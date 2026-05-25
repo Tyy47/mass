59c8b917ab4e087708be2907b3bf49bd1ea1cae6
Tyy47 <Tylerwmack321@gmail.com>
2026-05-25 15:20:38 -0400

    docs: added documentation to functions and variables.

--- files/files.go
+++ files/files.go
@@ -7,28 +7,35 @@ import (
 	"strconv"
 )
 
+// A list of directories that shouldn't be displayed or written
+var bannedDirectories = []string{}
+
+// File structure of how to write to json output
 type File struct {
 	path string
 	size float64
 }
 
+// Temp code storage to write them to json
 type FileStorage struct {
 	database []File
 }
 
+// Method to add files to file database to later write to json
 func (db *FileStorage) addToDatabase(file File) {
 	db.database = append(db.database, file)
 }
 
+// Returns a string that contains the value of megabytes in a file
 func (f File) SizeString() string {
 	return strconv.FormatFloat(f.size, 'f', -1, 64) + "mb"
 }
 
-
+// Declaring temp file database
 var FileDatabase = FileStorage{}
 
 
-func ReadCurrentDirectory() {
+func ReadDirectory() {
 	items, err := os.ReadDir(".")
 	if err != nil {
 		log.Fatalf(err.Error())
--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 
 
 func main() {
-	files.ReadCurrentDirectory()
+	files.ReadDirectory()
 }
