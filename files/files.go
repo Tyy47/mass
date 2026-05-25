3ee93eca3c9400ae2083c88d3862e870aa126ac3
Tyy47 <Tylerwmack321@gmail.com>
2026-05-25 15:17:50 -0400

    refactor: fixed recursive issues with reading directories

--- files/files.go
+++ files/files.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 type File struct {
@@ -19,20 +20,39 @@ func (db *FileStorage) addToDatabase(file File) {
 	db.database = append(db.database, file)
 }
 
+func (f File) SizeString() string {
+	return strconv.FormatFloat(f.size, 'f', -1, 64) + "mb"
+}
+
 
 var FileDatabase = FileStorage{}
 
 
 func ReadCurrentDirectory() {
-	item, err := os.ReadDir(".")
+	items, err := os.ReadDir(".")
 	if err != nil {
 		log.Fatalf(err.Error())
 		return
 	}
 
-	for _, entry := range item {
-		fmt.Println(entry)
+
+	var dirs []string
+	for _, entry := range items {
+		if entry.IsDir() {
+			dirs = append(dirs, entry.Name())
+		} else {
+			info, _ := os.Stat(entry.Name())
+			file := File{
+				path: entry.Name(),
+				size: float64(info.Size()) / (1024 * 1024),
+			}
+			fmt.Println(file.path + ": " + file.SizeString())
+		}
 	}
 
-	fmt.Println("\n Function complete")
+	for _, dir := range dirs {
+		os.Chdir(dir)
+		ReadCurrentDirectory()
+		os.Chdir("..")
+	}
 }
--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"mass/files"
-	"mass/utils"
 )
 
 
 func main() {
-	fmt.Println(utils.InitialDirectory)
 	files.ReadCurrentDirectory()
 }
